19e447cdf3bee80ebcd317f742b400035a8e037a
DmitryFedoroff <fedoroffx@gmail.com>
2024-12-20 08:06:44 +0700

    feat: Execute application with configuration and server setup
    #comment Start application by loading configuration and initializing the server
    
    Affected: main.go

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"log"
+
+	"project_sem/internal/server"
+	"project_sem/platform/config"
+)
+
+func main() {
+	settings, err := config.Load("")
+	if err != nil {
+		log.Fatalf("failed to load configuration: %v", err)
+	}
+
+	app := server.New(settings)
+	app.Run()
+}
